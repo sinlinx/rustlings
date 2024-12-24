bac52d36ade520c2c50ddb6c6de8938030892811
sinlinx <alex@sinlinx.com>
2024-12-24 21:04:38 +0800

    41

--- exercises/04_primitive_types/primitive_types2.rs
+++ exercises/04_primitive_types/primitive_types2.rs
@@ -1,7 +1,7 @@
 // Characters (`char`)
 
 fn main() {
-    // Note the _single_ quotes, these are different from the double quotes
+
     // you've been seeing around.
     let my_first_initial = 'C';
     if my_first_initial.is_alphabetic() {
@@ -17,7 +17,7 @@ fn main() {
     // Try a letter, try a digit (in single quotes), try a special character, try a character
     // from a different language than your own, try an emoji 😉
     // let your_character = '';
-
+    let your_character =  '😉';
     if your_character.is_alphabetic() {
         println!("Alphabetical!");
     } else if your_character.is_numeric() {
@@ -26,3 +26,4 @@ fn main() {
         println!("Neither alphabetic nor numeric!");
     }
 }
+    // Note the _single_ quotes, these are different from the double quotes
